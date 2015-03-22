aef643fad00162491ce96ed09184f71a6fef8b03
rathbunkm <rathbunkm@gmail.com>
2015-03-22 10:19:01 -0400

    Adding completed code for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The following functions are used to calculate the inverse of a matrix
+#and cache the result. If the inverse has already been calculated
+#the cached inverse will be called, rather than calculating 
+#the inverse again
 
-## Write a short comment describing this function
+#makeCacheMatrix calculates the inverse of an input matrix (x).
+#It then stores both the inverse and the matrix in as variables
+#in the parent environment (for calling in cacheSolve)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #calculate the inverse  
+  xinv<<- solve(x)
+  #cache matrix used to calculate the inverse as new variable
+  y <<- x
 }
 
 
-## Write a short comment describing this function
+#cacheSolve will do 1 of 2 things, under certain conditions. First,
+#if y=x, this means that the original matrix(x) has not been changed
+#since running 'makeCacheMatrix', which means the inverse has already
+#been calculated and has not changed. In this case, the cached inverse
+#is called and returned, along with a message explaining the retrieval.
+#However, if y is different from x, this means x has changed, and so
+#a new inverse must be calculated. In this case, messages are displayed
+#explaining that the matrix has changed and a new inverse is being
+#calculated. Then, the inverse is calculated, stored, and y is reset
+#in the parent environment for the next time 'cacheSolve' is called.
+#This effectively makes 'makeCacheMatrix' useless, since 'cacheSolve'
+#does the same steps, plus added functionality. It seemed better
+#that way, but I wrote 'makeCacheMatrix' anyways, because the
+#assignment required it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  #check if matrix is the same as previous runs 
+  if(all(y == x)) {
+      #retieve cached matrix and make action explicit via message
+      message("getting cached inverse")
+      return(xinv)
+  } 
+  
+  #if matrix is new, calculate new inverse
+  else {
+    #explain matrix is new and inverse is being calculated
+    message("matrix x has changed")
+    message("new inverse:")
+    #set new inverse in parent environment
+    xinv<<- solve(x)
+    #store matrix used to calculate inverse in parent environemt
+    y <<- x
+    return(xinv)
+  }
 }
